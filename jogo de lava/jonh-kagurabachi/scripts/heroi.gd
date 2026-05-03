1134c73802b44f1b666ed625d13db699cde447ea
Vinicius Eduardo <vnvnpa.oficial@gmail.com>
2026-05-03 18:04:52 -0300

    comit 2

---
+++ jogo de lava/jonh-kagurabachi/scripts/heroi.gd
@@ -0,0 +1,27 @@
+extends CharacterBody2D
+
+signal game_over
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -700.0
+
+
+func _physics_process(delta: float) -> void:
+	
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	if Input.is_action_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	var direction := Input.get_axis("ui_left", "ui_right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
+
+
+func _on_area_2d_area_entered(area: Area2D) -> void:
+	game_over.emit()
---
+++ jogo de lava/jonh-kagurabachi/scripts/main.gd
@@ -0,0 +1,19 @@
+extends Node2D
+
+
+const PLATE_SCENE = preload("res://scenes/plate.tscn")
+
+func _on_heroi_game_over() -> void:
+	print("game over")
+	get_tree().call_deferred("reload_current_scene")
+
+
+func _on_timer_timeout() -> void:
+	var plate: Node2D = PLATE_SCENE.instantiate()
+	
+	var screen_size = get_viewport_rect().size
+	
+	plate.position = Vector2(randf_range(708, 128) , 0)
+	
+	add_child(plate)
+ 
